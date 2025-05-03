52c3b0da2af4dba05f7c766e1e429ce19deeeb1c
Anton Bilbaeno <4b11b4@gmail.com>
2025-05-03 09:53:38 -0700

    Fix date inconsistency in GitHub Actions
    - Fixed issue where all entries showed the same date in GitHub Pages
    
    - Implemented git-based date extraction to get both creation and modification dates
    
    - Updated templates to display both created and modified dates
    
    - Modified GitHub workflow to fetch complete git history
    
    - Removed deprecated date field in favor of created_date and modified_date

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import re
 import datetime
 import markdown
+import subprocess
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
@@ -19,16 +20,62 @@ def title_from_filename(filename):
     return " ".join(word.capitalize() for word in basename.split("-"))
 
 
+def get_git_dates(filepath):
+    """Get creation and modification dates from git history."""
+    try:
+        # Get the first commit date (creation date)
+        creation_cmd = [
+            "git",
+            "log",
+            "--follow",
+            "--format=%ad",
+            "--date=iso",
+            "--reverse",
+            filepath,
+        ]
+        creation_date_str = (
+            subprocess.check_output(creation_cmd, text=True).strip().split("\n")[0]
+        )
+        creation_date = datetime.datetime.fromisoformat(
+            creation_date_str.replace(" ", "T").replace(" -", "-")
+        )
+
+        # Get the last commit date (modification date)
+        modified_cmd = ["git", "log", "-1", "--format=%ad", "--date=iso", filepath]
+        modified_date_str = subprocess.check_output(modified_cmd, text=True).strip()
+        modified_date = datetime.datetime.fromisoformat(
+            modified_date_str.replace(" ", "T").replace(" -", "-")
+        )
+
+        return {
+            "created": creation_date.strftime("%Y-%m-%d"),
+            "modified": modified_date.strftime("%Y-%m-%d"),
+        }
+    except (subprocess.SubprocessError, IndexError):
+        # Fallback to file system dates if git command fails
+        modified_time = os.path.getmtime(filepath)
+        modified_date = datetime.datetime.fromtimestamp(modified_time).strftime(
+            "%Y-%m-%d"
+        )
+        # Since we can't get creation from file system easily, use the same date
+        return {"created": modified_date, "modified": modified_date}
+
+
 def get_entry_metadata(filepath):
     """Extract metadata from an entry file."""
     filename = os.path.basename(filepath)
     title = title_from_filename(filename)
 
-    # Get last modified time
-    modified_time = os.path.getmtime(filepath)
-    date = datetime.datetime.fromtimestamp(modified_time).strftime("%Y-%m-%d")
+    # Get dates from git history
+    dates = get_git_dates(filepath)
 
-    return {"title": title, "date": date, "slug": slugify(title), "filepath": filepath}
+    return {
+        "title": title,
+        "created_date": dates["created"],
+        "modified_date": dates["modified"],
+        "slug": slugify(title),
+        "filepath": filepath,
+    }
 
 
 def get_all_entries(entries_dir):
@@ -41,8 +88,8 @@ def get_all_entries(entries_dir):
         ):
             entries.append(get_entry_metadata(filepath))
 
-    # Sort by date, newest first
-    return sorted(entries, key=lambda x: x["date"], reverse=True)
+    # Sort by modified date, newest first
+    return sorted(entries, key=lambda x: x["modified_date"], reverse=True)
 
 
 def read_entry_content(filepath):
